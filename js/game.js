d8029636bb14b08154f84f5c8af449c6a69c7f1e
iemprashant <prashantgupta9876@gmail.com>
2020-06-23 00:05:23 +0530

    basic setup and assets

---
+++ js/game.js
@@ -0,0 +1,23 @@
+function load_images(){
+
+}
+function init(){
+}
+
+function draw(){
+    
+}
+
+function update(){
+    
+}
+
+function gameloop(){
+    draw();
+    update();
+    console.log("gameloop");
+}
+
+load_images();
+init();
+var f =setInterval(gameloop,100);
