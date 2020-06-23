736235d02f0d8c4e2e6812e9cbde24e8dde13a8f
iemprashant <prashantgupta9876@gmail.com>
2020-06-23 15:30:02 +0530

    added collsion and score

--- js/game.js
+++ js/game.js
@@ -16,6 +16,7 @@ function init(){
     canvas.width=W;
 
     pen=canvas.getContext('2d');
+    game_over=false;
 
     e1={
         x:150,
@@ -25,14 +26,14 @@ function init(){
         speed:20
     };
     e2={
-        x:300,
+        x:350,
         y:150,
         w:60,
         h:60,
         speed:30
-    };
+    }
     e3={
-        x:450,
+        x:550,
         y:20,
         w:60,
         h:60,
@@ -45,7 +46,8 @@ function init(){
         w:60,
         h:60,
         speed:20,
-        moving:false
+        moving:false,
+        health:100,
     };
     gem={
         x:W-100,
@@ -59,6 +61,17 @@ function init(){
     canvas.addEventListener('mouseup',function(e){
         player.moving=false;
     })
+}
+
+function iscollision(rect1,rect2){
+    if (rect1.x < rect2.x + rect2.w &&
+        rect1.x + rect1.w > rect2.x &&
+        rect1.y < rect2.y + rect2.h &&
+        rect1.y + rect1.h > rect2.y) {
+         // collision detected!
+         return true;
+     }
+     return false;
 }
 
 function draw(){
@@ -75,15 +88,33 @@ function draw(){
     for(let i=0; i<enemy.length;i++){    
         pen.drawImage(enemy_image,enemy[i].x,enemy[i].y,enemy[i].w,enemy[i].h);
     };
-    
-    
+    pen.fillStyle="white";
+    pen.fillText("Score"+player.health,10,10)
 }
 
 function update(){
+    //player movement
     if(player.moving==true){
         player.x += player.speed;
+        player.health+=20;
     }
-    //movement
+    //collision btwn player and gem
+    if(iscollision(player,gem)==true){
+        alert("you Win");
+        game_over=true;
+        return;
+    }
+    for(let i=0; i<enemy.length;i++){    
+        if(iscollision(enemy[i],player)==true){
+            player.health-=50;
+            if(player.health<0){
+                game_over=true;
+                alert("Game Over" + player.health)
+            }
+            return;
+        }
+    }
+    // enemy movement
     for(let i=0; i<enemy.length;i++){    
         enemy[i].y +=  enemy[i].speed;
         if(enemy[i].y>H-enemy[i].h|| enemy[i].y<0){
@@ -93,6 +124,9 @@ function update(){
 }
 
 function gameloop(){
+    if(game_over==true){
+        clearInterval(f); 
+    }
     draw();
     update();
     console.log("gameloop");
