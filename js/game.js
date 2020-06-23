8f9d4d9ab7d1e0daa5fab590355c4af6dfca571b
iemprashant <prashantgupta9876@gmail.com>
2020-06-23 13:31:46 +0530

    added enemies

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 function load_images(){
-
+    enemy_image=new Image;
+    enemy_image.src="Assets/v1.png";
 }
 function init(){
     canvas=document.getElementById("mycanvas");
@@ -10,29 +11,47 @@ function init(){
 
     pen=canvas.getContext('2d');
 
-    box ={
+    e1={
         x:150,
         y:50,
         w:60,
         h:60,
-        speed:10
+        speed:20
     };
+    e2={
+        x:300,
+        y:150,
+        w:60,
+        h:60,
+        speed:30
+    };
+    e3={
+        x:450,
+        y:20,
+        w:60,
+        h:60,
+        speed:40
+    };
+    enemy=[e1,e2,e3];
 }
 
 function draw(){
     
     //clear previous box
     pen.clearRect(0,0,W,H);
-    //create new box
-    pen.fillStyle="red";
-    pen.fillRect(box.x,box.y,box.w,box.h);
+
+    for(let i=0; i<enemy.length;i++){    
+        pen.drawImage(enemy_image,enemy[i].x,enemy[i].y,enemy[i].w,enemy[i].h);
+    }
 }
 
 function update(){
     //movement
-    box.y+=box.speed;
-    if(box.y>H-box.h|| box.y<0){
-        box.speed*=-1;
+    for(let i=0; i<enemy.length;i++){    
+        enemy[i].y +=  enemy[i].speed;
+        if(enemy[i].y>H-enemy[i].h|| enemy[i].y<0){
+            enemy[i].speed*=-1;
+        }
     }
 }
 
